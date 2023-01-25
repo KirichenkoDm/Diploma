a8471aafbe917e6977819f166a02ccc4b93a4824
KirichenkoDm <120568362+KirichenkoDm@users.noreply.github.com>
2023-01-25 19:04:18 +0200

    Delete BackEnd/dist directory

--- BackEnd/dist/Courses/DTO/create-course.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { Topics } from "../topics";
-export declare class CreateCourseDto {
-    readonly hidden: boolean;
-    readonly name: string;
-    readonly topic: Topics;
-    readonly description: string;
-    readonly matherials: string;
-}
--- BackEnd/dist/Courses/course.interface.d.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-import { Topics } from './topics';
-export interface ICourse extends Document {
-    readonly hidden: boolean;
-    readonly name: string;
-    readonly topic: Topics;
-    readonly description: string;
-    readonly matherials: string;
-}
